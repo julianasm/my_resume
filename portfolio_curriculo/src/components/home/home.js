3f759c8ad59b65771926f25d96c7c82d8f805eb4
juliana <julianasouzamelo@live.com>
2021-04-03 14:11:28 -0300

    configurando deploy para github pages

--- portfolio_curriculo/src/components/home/home.js
+++ portfolio_curriculo/src/components/home/home.js
@@ -28,12 +28,12 @@ function Home() {
               Experiência profissional
             </h3>
             <p className="description">
-            <NewlineText text={"Supergeeks - 1ª Escola de Programação e Robótica do Brasil\n" +
-            "Instrutora de programação para crianças e adolescentes\n"+
-            "Out/2020 - Mar/2021 \n" +
-            "Colégio Dromos\n"+
-            "Monitoria de Ciências e Biologia\n" +
-            "Jun/2018 - Abr/2020 \n"}/>
+            <NewlineText text={"Empresa: Supergeeks - 1ª Escola de Programação e Robótica do Brasil\n" +
+            "Atribuições: Instrutora de programação para crianças e adolescentes\n"+
+            "Período: Out/2020 - Mar/2021 \n" +
+            "Empresa: Colégio Dromos\n"+
+            "Atribuições: Monitoria de Ciências e Biologia\n" +
+            "Período: Jun/2018 - Abr/2020 \n"}/>
             </p>
             <h3 className="academic">
                 Formação acadêmica
@@ -41,6 +41,12 @@ function Home() {
              <p className="description2">
               <NewlineText text={"Curso: Anlálise e Desenvolvimento de Sistemas\n" + "Institução: Estácio \n" + "Ago/2020 - Dez/2022 \n" + "Curso: Ciências Biológias \n" + "Instituição: Universidade de Brasília \n" + "Mar/2016 - Dez/2020 \n"} />
              </p>
+             <h3 className="habilities"> 
+              Habilidades técnicas
+             </h3>
+             <p className="description3">
+                <NewlineText text={"Conhecimentos básicos:\n" + "ReactJs, CSS3, HTML5, MySQL, Git/Github, Dart \n" + "Conhecimentos intermediários:\n" + "Python, C++ \n" + "Pacote Office: Word, Excel, PowerPoint;\n" + "Google Drive(Planilhas, Docs, Apresentações)\n" + "Inglês (B2 - Intermediário superior)\n"}/>
+              </p>
         </body>
     </div>
   );
