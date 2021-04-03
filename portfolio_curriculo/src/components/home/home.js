0982e46daabc5cdc4e233c07cfe96b914acb0f62
juliana <julianasouzamelo@live.com>
2021-04-03 10:39:08 -0300

    adding a new box with academic info

--- portfolio_curriculo/src/components/home/home.js
+++ portfolio_curriculo/src/components/home/home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import "../home/home.css";
 
+function NewlineText(props) {
+  const text = props.text;
+  const newText = text.split('\n').map(str => <p>{str}</p>)
+  return newText;
+}
+
 function Home() {
   return (
     <div className="Homepage">
         <header className="HomeHeader">
             <p1 className="contact">
-              Juliana Souza de Melo
+              Juliana Souza
               julianasouzamelo2@gmail.com
               +55 61 981062410</p1>
               <a className="links" href="https://github.com/julianasm">Github</a>
@@ -21,6 +27,20 @@ function Home() {
             <h3 className="experiences">
               Experiência profissional
             </h3>
+            <p className="description">
+            <NewlineText text={"Supergeeks - 1ª Escola de Programação e Robótica do Brasil\n" +
+            "Instrutora de programação para crianças e adolescentes\n"+
+            "Out/2020 - Mar/2021 \n" +
+            "Colégio Dromos\n"+
+            "Monitoria de Ciências e Biologia\n" +
+            "Jun/2018 - Abr/2020 \n"}/>
+            </p>
+            <h3 className="academic">
+                Formação acadêmica
+            </h3>
+             <p className="description2">
+              <NewlineText text={"Curso: Anlálise e Desenvolvimento de Sistemas\n" + "Institução: Estácio \n" + "Ago/2020 - Dez/2022 \n" + "Curso: Ciências Biológias \n" + "Instituição: Universidade de Brasília \n" + "Mar/2016 - Dez/2020 \n"} />
+             </p>
         </body>
     </div>
   );
