c169c165640a199433ff457964af6e2d32eb8f12
juliana <julianasouzamelo@live.com>
2021-04-04 08:22:15 -0300

    adding router configuration

--- portfolio_curriculo/src/App.js
+++ portfolio_curriculo/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import Home from "../src/components/home/home"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
 
 function App() {
   return (
-    <Route path="src/components/home/home.js">
     <Home />
-    </Route>
-    
   );
 }
+
 
 export default App;
