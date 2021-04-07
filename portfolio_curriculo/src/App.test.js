c6065488cdff56f18d2c87e171c8fc9ad06d813b
juliana <julianasouzamelo@live.com>
2021-04-07 02:01:13 -0300

    atualizando json para o deploy

--- portfolio_curriculo/src/App.test.js
+++ portfolio_curriculo/src/App.test.js
@@ -1,9 +1,10 @@
 import BrowserRouter from 'react-router-dom/BrowserRouter'
 import App from './App';
 
-ReactDOM.render((
+/* ReactDOM.render((
    <BrowserRouter basename={process.env.PUBLIC_URL}>
      <App />
    </BrowserRouter>
     <Route exact path='/' render="/home/juliana/react_test/portfolio_curriculo/src/App.js"/>} />
-), ...)  
+), ...) 
+*/
